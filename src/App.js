38535638255c8933d14f539ab383840eb6b6ac18
Yongseung Kim <kimche9865@naver.com>
2024-03-27 22:42:58 +0900

    use Effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKey] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKey(event.target.value);
+  console.log("i run all time");
+  useEffect(() => {
+    console.log("asd");
+  },[])
+  useEffect(() =>{
+  if(keyword !== "" && keyword.length>=5)
+  console.log("searchfor",keyword)
+  },[keyword]);
   return (
     <div >
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text={"Countinue"}/>
+      <input 
+        value={keyword}
+        onChange={onChange}
+        type="text" 
+        placeholder="search here"></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick} >click me</button>
     </div>
   );
 }
